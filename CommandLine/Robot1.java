f7846b75c6c75abf8ad10954142f7e296670122d
elizabeth.hillman <elizabethhillman0@gmail.com>
2018-10-08 08:55:24 -0700

    created methods for robot to move head and have blinking lights

--- CommandLine/Robot1.java
+++ CommandLine/Robot1.java
@@ -3,6 +3,33 @@ import edu.cmu.ri.createlab.hummingbird.HummingbirdRobot;
 
 //if we want to pause use Thread.sleep and use an exception
 public class Robot1 extends HummingbirdRobot {
+    HummingbirdRobot athena = new HummingbirdRobot();
 
+    public void blinkLights(int port, int red, int green, int blue){
+        for(int i = 0; i<= 50; i++){
+            athena.setFullColorLED(port, red, green, blue);
+
+            //wait
+            try { Thread.sleep(200);
+            } catch (InterruptedException e){}
+
+            //turn off colors
+            athena.setFullColorLED(port, 0, 0, 0);
+            //wait
+            try { Thread.sleep(200);
+            } catch (InterruptedException e){}
+        }
+    }
+    public void moveHead(int pos){
+        athena.setServoPosition(1, pos);
+    }
+
+    public void resetHead(){
+        athena.setServoPosition(1, 127);
+    }
+
+    public void speak(String talk){
+        athena.speak(talk);
+    }
 
 }
